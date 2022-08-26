e15cffdbff85298a958f0590961850e7bd029b94
john <john@cheme.kyoto-u.ac.jp>
2022-08-26 19:10:46 +0900

    Read token/license from file

--- main.cpp
+++ main.cpp
@@ -12,15 +12,18 @@ using namespace web::json;
 using namespace utility;
 
 // Verify a license key using the validate-key action
-pplx::task<http_response> validate_license_key(http_client client, const string license_key)
+pplx::task<http_response> validate_license_key(http_client client, const string activation_token,
+                                               const string license_key)
 {
   http_request req;
 
   value meta;
   meta["key"] = value::string(license_key);
+  //meta["scope"]["fingerprint"] = ;
 
   value body;
   body["meta"] = meta;
+
 
   req.headers().add("Content-Type", "application/vnd.api+json");
   req.headers().add("Accept", "application/json");
@@ -32,19 +35,36 @@ pplx::task<http_response> validate_license_key(http_client client, const string 
   return client.request(req);
 }
 
+json readlicensefile(string const &filepath){
+  value license;
+  try{
+    ifstream fp(filepath);
+    stringstream str;
+    str << fp.rdbuf();
+    fp.close();
+    license = value::parse(str);
+  }catch(json_exception &e){
+    cerr << "Error reading license file " << filepath << endl;
+    exit(-1);
+  }
+  return license
+}
+
 int main(int argc, char* argv[])
 {
   if (argc == 1)
   {
     cerr << "[ERROR] "
-         << "No license key argument specified"
+         << "No license key file specified"
          << endl;
 
     exit(1);
   }
 
-  string account_id = getenv("KEYGEN_ACCOUNT_ID");
-  string license_key = argv[1];
+  const string account_id = getenv("KEYGEN_ACCOUNT_ID");
+  cerr << "reading license file" << endl; 
+  json license = readlicensefile(argv[1]);
+  cerr << "key = " << license_key << endl;
   http_client client(uri("https://api.keygen.sh/v1/accounts/" + account_id));
 
   validate_license_key(client, license_key)
@@ -69,11 +89,12 @@ int main(int argc, char* argv[])
       auto data = json.at("data");
       auto meta = json.at("meta");
 
+      cout << meta.serialize() << endl;      
       if (meta.at("valid").as_bool())
       {
         cout << "[OK] "
              << "License key is valid: "
-             << "code=" << meta.at("code").as_string() << " "
+          //<< "code=" << meta.at("code").as_string() << endl;/*<< " "
              << "id=" << data.at("id").as_string()
              << endl;
       }
---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clang++ main.cpp -o bin.out \
+        -std=c++17 \
+        -stdlib=libc++ \
+        -lcpprest \
+        -I/opt/homebrew/include \
+        -L/opt/homebrew/lib \
+        -lboost_system \
+        -lboost_thread-mt \
+        -lboost_chrono-mt \
+        -lssl \
+        -lcrypto \
+        -I/opt/homebrew/opt/openssl/include \
+        -L/opt/homebrew/opt/openssl/lib \
+        -I/opt/homebrew/opt/cpprestsdk/include \
+        -L/opt/homebrew/opt/cpprestsdk/lib
+
+./bin.out 'kapsel_license'
+
